072294c5368ece7204a378dfa13f4cc386b284d2
Muhammad Sharjeel Farzad <msharjeelfarzad@gmail.com>
2025-08-07 18:49:08 +0500

    Added Assignment-04

---
+++ Assignment-03/Assignment-03/Factories/Factory.cs
@@ -0,0 +1,111 @@
+﻿using Assignment_03.Models;
+using Assignment_03.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_03.Factories
+{
+    public abstract class Product
+    {
+
+        public void getProductInfo()
+        {   
+            var product = createProduct();
+            Console.WriteLine("The information of the poruct is as follows: -");
+            Console.WriteLine($"Product Description: {product.getDescription()}");
+            Console.WriteLine($"Product Type: {product.getType()}");
+            Console.WriteLine($"Product Price: {product.getPrice()}");
+            Console.WriteLine($"Product Quantity: {product.getQuantity()}");
+            Console.WriteLine($"Product ID: {product.getID()}");
+        }
+        
+        public abstract IProductFeatures createProduct();
+        public abstract IProductFeatures GetProduct();
+    }
+
+    public class Clothing : Product
+    {
+        public IProductFeatures Instance;
+        public Clothing(string name)
+        {
+            string Name=name.ToLower();
+            switch (Name)
+            {
+                case "coat": Instance = new Coat(); break;
+                case "dress shirt": Instance = new DressShirt(); break;
+             
+
+            }
+        }
+        public override IProductFeatures createProduct() => Instance;
+        public override IProductFeatures GetProduct() => Instance;
+    }
+    public class Electronics : Product
+    {
+        public IProductFeatures Instance;
+        public Electronics(string name)
+        {
+            string Name = name.ToLower();
+            switch (Name)
+            {
+                case "fridge": Instance = new Fridge(); break;
+                case "air conditioner": Instance = new AirConditioner(); break;
+                case "computer": Instance = new Computer(); break;
+
+
+            }
+        }
+        public override IProductFeatures createProduct() => Instance;
+        public override IProductFeatures GetProduct() => Instance;
+
+    }
+
+
+    public class HomeGarden : Product
+    {
+        public IProductFeatures Instance;
+        public HomeGarden(string name)
+        {
+            string Name = name.ToLower();
+            switch (Name)
+            {
+                case "mango garden": Instance = new MangoGarden(); break;
+                case "apple garden": Instance = new AppleGarden(); break;
+
+            }
+        }
+        public override IProductFeatures createProduct() => Instance;
+        public override IProductFeatures GetProduct() => Instance;
+
+    }
+
+    public class Book : Product
+    {
+        public IProductFeatures Instance;
+        public Book(string name)
+        {
+            string Name = name.ToLower();
+            switch (Name)
+            {
+                case "math book": Instance = new MathBook(); break;
+                case "english book": Instance = new EnglishBook(); break;
+
+            }
+        }
+        public override IProductFeatures createProduct() => Instance;
+        public override IProductFeatures GetProduct() => Instance;
+
+    }
+
+
+
+}
+
+
+
+
+
---
+++ Assignment-03/Assignment-03/Models/ProductManager.cs
@@ -0,0 +1,73 @@
+﻿using Assignment_03.Factories;
+using Assignment_03.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_03.Models
+{
+    public class ProductManager : IProductRepository
+    {
+        // Inventory
+        public static List<Product> products = new List<Product>() { };
+        public ProductManager() { 
+        
+        generateProducts();
+        }
+        public List<Product> GetByCategory(string category)
+        {
+            List<Product> filtered=products.Where(product=>product.GetProduct().getType()==category).ToList();
+            return filtered;
+
+        }
+        public List<Product> GetProductByID(string id) {
+            List<Product> filtered = products.Where(product => product.GetProduct().getID() == id).ToList();
+            return filtered;
+        }
+        public List<Product> GetAllProducts(string category)
+        {
+            
+            return products;
+
+        }
+
+        public List<Product> GetLowStockProducts(int threshold)
+        {
+            List<Product> filtered = products.Where(product => product.GetProduct().getQuantity() <=threshold).ToList();
+            return filtered;
+        }
+        void generateProducts()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Product product_1 = new Clothing("dress shirt");
+                Product product_2 = new Clothing("coat");
+                Product product_3 = new Electronics("fridge");
+                Product product_4 = new Electronics("computer");
+                Product product_5 = new Electronics("air conditioner");
+                Product product_6 = new HomeGarden("apple garden");
+                Product product_7 = new HomeGarden("mango garden");
+                Product product_8 = new Book("math book");
+                Product product_9 = new Book("english book");
+                products.Add(product_1);
+                products.Add(product_2);
+                products.Add(product_3);
+                products.Add(product_4);
+                products.Add(product_5);
+                products.Add(product_6);
+                products.Add(product_7);
+                products.Add(product_8);
+                products.Add(product_9);
+
+
+            }
+        }
+
+
+
+    }
+
+
+}
